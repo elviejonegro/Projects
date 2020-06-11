d03bf9466378b70d4ac13da39783329b7bf9164e
Jose Vicente Ayala Luna <jayala@starbucks.com>
2020-06-10 23:18:42 -0500

    adding example mapping

--- src/main/java/com/delivery/address/controller/TestController.java
+++ src/main/java/com/delivery/address/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.delivery.address.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController
+@RequestMapping("/testDelivery")
 public class TestController {
 
     @GetMapping("/test")
